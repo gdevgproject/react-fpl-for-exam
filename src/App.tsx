5bc4d8b78f461cebe29409b58457aa4076989113
gdevgproject <gdev.gproject@gmail.com>
2025-01-10 23:43:24 +0700

    feat: button to get API using fetch method

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactDOM, useEffect, useState } from "react";
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // Góc nhìn người dùng
+  // 1. Bấm vào button
+  // 2. Nhìn thấy list sản phẩm hiện ra
+
+  // Góc nhìn dev
+  // 1. Tạo nút button
+  // 2. Gọi API để lấy sản phẩm khi được ấn nút
+  // 3. Lưu dữ liệu và state
+  // 4. Hiển thị sản phẩm
+  const [products, setProducts] = useState([]);
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/products");
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  function showItemHandler() {
+    setIsShow(!isShow);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="container">
+        <button className="toggle-btn" onClick={showItemHandler}>
+          {isShow ? "Ẩn sản phẩm" : "Xem sản phẩm"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {isShow && (
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>Tên sản phẩm</th>
+                <th>Giá</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <Item
+                  key={product.id}
+                  name={product?.name}
+                  price={product?.price}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function Item({ name, price }) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{price}</td>
+    </tr>
+  );
+}
