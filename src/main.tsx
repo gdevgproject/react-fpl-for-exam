9accbd1e5535497425a5c2b5676ae8aeabe9d58c
gdevgproject <gdev.gproject@gmail.com>
2025-02-13 08:39:46 +0700

    feat: CRUD login logout

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import App, { productAction } from './App.tsx'
+import App from './App.tsx'
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    action: productAction
-  }
-])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 )
